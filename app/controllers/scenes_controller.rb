f23dee227c88d4fbfaf3509d59e9f1d23956596b
Kelsey Langlois and L. Devin MacKrell <kelsey+l.@>
2017-11-15 13:59:05 -0800

    Add edit and delete for scene.

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -20,6 +20,27 @@ class ScenesController < ApplicationController
     @scene = Scene.find(params[:id])
   end
 
+  def edit
+    @scene = Scene.find(params[:id])
+  end
+
+  def update
+    @scene = Scene.find(params[:id])
+    if @scene.update(scene_params)
+      flash[:notice] = "Scene updated!"
+      redirect_to scene_path(@scene)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @scene = Scene.find(params[:id])
+    @scene.destroy
+    flash[:notice] = "Scene has been taken by the Demogorgon."
+    redirect_to root_path
+  end
+
 private
   def scene_params
     params.require(:scene).permit(:description, :episode_id, :plotline_id)
