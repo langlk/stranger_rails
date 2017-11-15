5b4d6aac399e0b59d095f9ccc363c0a995824755
Kelsey Langlois and L. Devin MacKrell <kelsey+l.@>
2017-11-15 14:18:50 -0800

    Add intersect function to find scenes that match a plot and ep.

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -2,4 +2,6 @@ class Scene < ActiveRecord::Base
   belongs_to :episode
   belongs_to :plotline
   validates :episode, :plotline, :description, presence: true
+
+  scope :intersect, -> (scene_plot, scene_ep) { where(plotline: scene_plot, episode: scene_ep) }
 end
--- spec/models/scene_spec.rb
+++ spec/models/scene_spec.rb
@@ -7,4 +7,17 @@ describe Scene do
 
   it { should belong_to :episode }
   it { should belong_to :plotline }
+
+  describe ".intersect" do
+    it "should return the scenes that belong to both the plotline and the episode provided" do
+      e1 = Episode.create(title: "Test1", season: 1, number: 1, rating: 0)
+      e2 = Episode.create(title: "Test2", season: 2, number: 2, rating: 0)
+      p1 = Plotline.create(name: "Test1", description: "Test1")
+      p2 = Plotline.create(name: "Test2", description: "Test2")
+      s1 = Scene.create(description: "Test1", plotline_id: p1.id, episode_id: e1.id)
+      s2 = Scene.create(description: "Test2", plotline_id: p2.id, episode_id: e2.id)
+      expect(Scene.intersect(p1, e1)).to eq([s1])
+      expect(Scene.intersect(p2, e2)).to eq([s2])
+    end
+  end
 end
