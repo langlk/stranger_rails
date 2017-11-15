e8e07c3b6f2c76d2ed64aee1b66d80beaa0766fc
Kelsey Langlois and L. Devin MacKrell <kelsey+l.@>
2017-11-15 10:47:27 -0800

    Add Plotline seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+plotlines = [
+  ['Eleven', 'Adventures of Eleven, Wunderkid.'],
+  ['Kids', 'Trials and tribulations of Mike, Lucas, Dustin, and associates.'],
+  ['Teens', 'Strange times at Hawkins High.'],
+  ['Will', 'Will! WILL! WILL!!!!!'],
+  ['Town Infection', "There's something wrong with the pumpkins."]
+]
+
+plotlines.each do |plot_name, plot_description|
+  Plotline.create(name: plot_name, description: plot_description)
+end
