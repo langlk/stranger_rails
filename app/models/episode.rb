a40e4a0d3e010bc7ed230d08287b7b4f8099fe81
Kelsey Langlois and L. Devin MacKrell <kelsey+l.@>
2017-11-15 09:57:27 -0800

    Add alphabetical scope to eps.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,5 @@ class Episode < ActiveRecord::Base
   validates :title, :season, :number, :rating, presence: true
 
   scope :chronological, -> { order(season: :asc, number: :asc) }
+  scope :alphabetical, -> { order(:title) }
 end
--- spec/models/episode_spec.rb
+++ spec/models/episode_spec.rb
@@ -29,4 +29,28 @@ describe Episode do
       expect(Episode.chronological).to eq([e1, e2, e3])
     end
   end
+
+  describe '.alphabetical' do
+    it "returns episodes sorted alphabetically by title" do
+      e1 = Episode.create(
+        title: "The Vanishing of Will Byers",
+        season: 1,
+        number: 1,
+        rating: 4
+      )
+      e3 = Episode.create(
+      title: "Madmax",
+      season: 2,
+      number: 1,
+      rating: 4
+      )
+      e2 = Episode.create(
+        title: "The Weirdo on Maple Street",
+        season: 1,
+        number: 2,
+        rating: 4
+      )
+      expect(Episode.alphabetical).to eq([e3, e1, e2])
+    end
+  end
 end
