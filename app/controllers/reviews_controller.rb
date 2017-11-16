ebc79128ca3680d3cc5d6e3caf6650098e1e0379
Kelsey Langlois and L Devin MacKrell <kelsey+l@>
2017-11-16 10:49:55 -0800

    Update app so that individuals can only edit or delete the reviews they authored.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,41 +14,65 @@ class ReviewsController < ApplicationController
 
   def new
     @episode = Episode.find(params[:episode_id])
-    @review = @episode.reviews.new
+    if current_user
+      @review = @episode.reviews.new
+    else
+      flash[:alert] = "You must be logged in to leave a review."
+      redirect_to episode_path(@episode)
+    end
   end
 
   def create
     @episode = Episode.find(params[:episode_id])
-    @review = @episode.reviews.new(review_params)
-    @review.user = current_user
-    if @review.save
-      flash[:notice] = "Review created!"
-      redirect_to review_path(@review)
+    if current_user
+      @review = @episode.reviews.new(review_params)
+      @review.user = current_user
+      if @review.save
+        flash[:notice] = "Review created!"
+        redirect_to review_path(@review)
+      else
+        render :new
+      end
     else
-      render :new
+      flash[:alert] = "You must be logged in to leave a review."
+      redirect_to episode_path(@episode)
     end
   end
 
   def edit
     @review = Review.find(params[:id])
+    if current_user != @review.user
+      flash[:alert] = "This isn't your review. No touchy."
+      redirect_to review_path(@review)
+    end
   end
 
   def update
     @review = Review.find(params[:id])
-    @episode = @review.episode
-    if @review.update(review_params)
-      flash[:notice] = "Review updated!"
+    if current_user != @review.user
+      flash[:alert] = "Seriously, kid. How did you get in here? This area is off-limits."
       redirect_to review_path(@review)
     else
-      render :edit
+      @episode = @review.episode
+      if @review.update(review_params)
+        flash[:notice] = "Review updated!"
+        redirect_to review_path(@review)
+      else
+        render :edit
+      end
     end
   end
 
   def destroy
     @review = Review.find(params[:id])
-    @review.destroy
-    flash[:notice] = "Review has gone to be with Barb."
-    redirect_to reviews_path
+    if current_user != @review.user
+      flash[:alert] = "Seriously, kid. How did you get in here? This area is off-limits."
+      redirect_to review_path(@review)
+    else
+      @review.destroy
+      flash[:notice] = "Review has gone to be with Barb."
+      redirect_to reviews_path
+    end
   end
 
 private
