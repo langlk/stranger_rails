415afe7c2327a8b02e0ed798df7e74d55cf28ab3
Kelsey Langlois and L Devin MacKrell <kelsey+l@>
2017-11-16 11:39:33 -0800

    Add recent reviews display

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :episode
   belongs_to :user
   validates :episode, :content, :rating, :user, presence: true
+
+  scope :recent, -> { where("created_at >=?", DateTime.now - 7.days).order(updated_at: :desc)}
+
 end
